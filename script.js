c374cd1aeb2feb9568cc18eff1a2e270e85886a4
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2023-09-10 16:58:31 +0200

    Added functions to board and displayBoard modules.

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
+const player = () => {
+
+    return {};
+};
+
 const gameBoard = (() => {
     const board = ['X', 'O', 'O', 'O', 'X', 'O', 'O', 'O', 'X'];
+    
+    const newSign = (index, sign) => {
+        board[index] = sign;
+    };
+
+    const getSign = (index) => {
+        return board[index];
+    }
+
+    const resetBoard = () => {
+        for (let i = 0; i < 9; i++) {
+            board[i] = '';
+        }
+    };
+
+    return { newSign, getSign, resetBoard };
+})();
+
+const displayController = (() => {
     const displayBoard = () => {
         for (let i = 0; i < 9; i++) {
             const currentSquare = document.getElementById(`gs${i}`);
-            currentSquare.textContent = board[i];
+            currentSquare.textContent = gameBoard.getSign(i);
         }
     };
+
     return { displayBoard };
 })();
-
-const player = () => {
-    
-    return {};
-};
 
 const gameSquares = document.querySelectorAll('.game-square');
 gameSquares.forEach(function(currentSquare) {
