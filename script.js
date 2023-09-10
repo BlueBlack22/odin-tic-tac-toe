3c772bca0faac249141dda11b96c8c17213dbe26
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2023-09-10 16:45:45 +0200

    Added event listener to game squares.

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const gameSquare = document.querySelectorAll('.game-square');
-
 const gameBoard = (() => {
     const board = ['X', 'O', 'O', 'O', 'X', 'O', 'O', 'O', 'X'];
     const displayBoard = () => {
-        for (let i = 1; i <= 9; i++) {
+        for (let i = 0; i < 9; i++) {
             const currentSquare = document.getElementById(`gs${i}`);
-            currentSquare.textContent = board[i-1];
+            currentSquare.textContent = board[i];
         }
     };
     return { displayBoard };
@@ -16,3 +14,7 @@ const player = () => {
     return {};
 };
 
+const gameSquares = document.querySelectorAll('.game-square');
+gameSquares.forEach(function(currentSquare) {
+    currentSquare.addEventListener('click', (e) => console.log(e.target.dataset.index));
+});
